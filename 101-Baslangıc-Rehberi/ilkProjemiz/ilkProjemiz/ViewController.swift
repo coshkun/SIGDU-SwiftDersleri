eaef3837a60100a6a740435a5b196013a048ba6d
Coskun Caner <cskncnr@gmail.com>
2020-07-13 00:59:33 +0300

    101-BaslangicKlavuzu-Ders1

---
+++ 101-Baslangıc-Rehberi/ilkProjemiz/ilkProjemiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ilkProjemiz
+//
+//  Created by Coskun Appwox on 12.07.2020.
+//  Copyright © 2020 Coskun Caner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
